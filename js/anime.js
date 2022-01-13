7993fc4d3a933b6da18f94315c2e0f508f5e373f
midukikobayashi <85478818+midukikobayashi@users.noreply.github.com>
2022-01-13 23:46:27 +0900

    js

--- js/anime.js
+++ js/anime.js
@@ -3,3 +3,16 @@ $(function(){
     $('.f-menu').toggleClass('is-active');
   });
 }());
+
+$(function() {
+  $(window).scroll(function() {
+    $(".scroll-block").each(function() {
+      var scroll = $(window).scrollTop();
+      var blockPosition = $(this).offset().top;
+      var windowHeihgt = $(window).height();
+      if (scroll > blockPosition - windowHeihgt + 300) {
+        $(this).addClass("blockIn");
+      }
+    });
+  });
+});
